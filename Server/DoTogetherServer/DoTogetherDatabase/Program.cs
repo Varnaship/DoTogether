3d03ee974f5b9f4c858b8e46edde1c21e7b88e2f
Kristian Petrov <kriskoplay05@gmail.com>
2025-08-18 20:59:03 +0300

    Made database,DTOs and constants

--- Server/DoTogetherServer/DoTogetherDatabase/Program.cs
+++ Server/DoTogetherServer/DoTogetherDatabase/Program.cs
@@ -1,11 +1,19 @@
+using AutoMapper;
+using DoTogetherDatabase.Data;
+using DoTogetherDatabase.Mapping;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Add services to the container.
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Register AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
+
+// Configure CORS for Angular
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAngularApp",
@@ -14,8 +22,12 @@ builder.Services.AddCors(options =>
                         .AllowAnyMethod());
 });
 
+builder.Services.AddDbContext<DoTogetherDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- Server/DoTogetherServer/DoTogetherDatabase/obj/Debug/net8.0/DoTogetherDatabase.AssemblyInfo.cs
+++ Server/DoTogetherServer/DoTogetherDatabase/obj/Debug/net8.0/DoTogetherDatabase.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoTogetherDatabase")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80674270a060a7911ede1901df78fc042f1e1d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoTogetherDatabase")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoTogetherDatabase")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
